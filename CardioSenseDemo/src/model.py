5ea70893b65129e99129bf9433e6f52fb3e6db90
Wichapat900 <soowichapat@gmail.com>
2026-02-08 16:09:36 +0700

    Update model.py

--- CardioSenseDemo/src/model.py
+++ CardioSenseDemo/src/model.py
@@ -5,7 +5,7 @@ class ECG_CNN(nn.Module):
     def __init__(self, num_classes=3):
         super().__init__()
 
-        self.features = nn.Sequential(
+        self.net = nn.Sequential(
             nn.Conv1d(1, 16, kernel_size=5, padding=2),
             nn.BatchNorm1d(16),
             nn.ReLU(),
@@ -19,17 +19,11 @@ class ECG_CNN(nn.Module):
             nn.Conv1d(32, 64, kernel_size=3, padding=1),
             nn.BatchNorm1d(64),
             nn.ReLU(),
-            nn.MaxPool1d(2)
-        )
+            nn.AdaptiveAvgPool1d(1),
 
-        self.classifier = nn.Sequential(
-            nn.Linear(64 * 16, 64),
-            nn.ReLU(),
-            nn.Dropout(0.5),
+            nn.Flatten(),
             nn.Linear(64, num_classes)
         )
 
     def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), -1)
-        return self.classifier(x)
+        return self.net(x)
